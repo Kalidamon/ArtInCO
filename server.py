019e7f0ae188eb87ca96aa2c689c93b651468123
Kalidamon <cristian.san.garcia@gmail.com>
2026-07-16 17:20:31 -0500

    Add extractor_args for YouTube client configuration

--- server.py
+++ server.py
@@ -268,6 +268,12 @@ def download_mp3():
                     'key': 'FFmpegMetadata',
                 },
             ],
+            'extractor_args': {
+                'youtube': {
+                    # Intenta forzar que se comporte como un cliente de Android o TV
+                    'player_client': ['android', 'ios'], 
+                }
+            },
             'quiet': False,
             'no_warnings': False,
             'ffmpeg_location': ffmpeg_dir,
